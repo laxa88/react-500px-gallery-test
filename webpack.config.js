b07d1330e0b6aa678d86f0a79c548a196d8f3160
LWY <gashzack@hotmail.com>
2017-12-25 22:47:18 +0900

    Add HtmlWebpackPlugin to auto-generate html and inject js when testing with webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,16 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+})
+
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: './dist/bundle.js'
-  }
+  },
+  plugins: [
+    HtmlWebpackPluginConfig
+  ]
 }
