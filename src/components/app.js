6949b68763210fff49c5ead4df8a14e4a65e18e4
laxa88 <gashzack@hotmail.com>
2017-12-30 20:19:58 +0800

    Add react-router, setup initial paths

--- src/components/app.js
+++ src/components/app.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import {Link, Route} from 'react-router-dom';
 
-/**
- * App
- */
-export default class App extends React.Component {
-  /**
-   * render
-   * @return {ReactDOM}
-   */
-  render() {
-    return (
-      <div style={{textAlign: 'center'}}>
-        <h1>Hello World</h1>
-      </div>
-    );
-  }
-}
+import Home from './home';
+import Search from './search';
+
+const App = () => (
+  <div>
+    <nav>
+      <Link to="/">Home</Link>
+    </nav>
+    <div>
+      <Route exact path="/" component={Home}/>
+      <Route path="/search" component={Search}/>
+    </div>
+  </div>
+);
+
+export default App;
