14cb97779e0256186b7f0a2e1160a41ba002fce4
laxa88 <gashzack@hotmail.com>
2017-12-30 19:44:23 +0800

    Lint webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,20 +2,20 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
   template: './src/index.html',
   filename: 'index.html',
-  inject: 'body'
-})
+  inject: 'body',
+});
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: './dist/bundle.js'
+    filename: './dist/bundle.js',
   },
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ }
-    ]
+      {test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/},
+    ],
   },
   plugins: [
-    HtmlWebpackPluginConfig
-  ]
-}
+    HtmlWebpackPluginConfig,
+  ],
+};
